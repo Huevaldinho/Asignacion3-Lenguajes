19ed3ee417cf737ca7b793be56310409b0c0bb25
mfss0412 <mfss0412@estudianTEC.cr>
2022-10-21 19:09:24 -0600

    Todo Junto
    Todo en un solo archivo

--- item5.go
+++ item5.go
@@ -7,6 +7,72 @@ import (
 
 // import "main/main.go" as tree
 
+//======================================Generador Aleatorio=========================================================
+
+func isPrime(number int) bool {
+	if number == 0 || number == 1 {
+		return false
+	} else {
+		for i := 2; i <= number/2; i++ {
+			if number%i == 0 {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// Funcion para optener la x
+func getX(sem int) int {
+	var candidato int
+	candidato = sem
+	//Se recorren posibles candidatos
+	for true {
+		//Se encontro un candidatos
+		if (isPrime(candidato)) && candidato > sem {
+			return candidato
+		}
+		candidato = candidato + 1
+	}
+	return -1
+}
+
+func generaAleatorio(semilla int, n int, list *[]int) {
+	var x, m, a, b int
+	x = getX(semilla)
+	m = 4096
+	a = 109
+	b = 853
+	for i := 0; i < n; i++ {
+		(*list)[i] = x
+		x = ((a*(x) + b) % m)
+	}
+}
+
+func convertidor(list *[]int) {
+	for i := 0; i < len(*list); i++ {
+		(*list)[i] = (*list)[i] % 255
+	}
+}
+
+func generaAleatorioAux(semilla, n int) []int {
+	var array [5000]int                 //Valor maximo del array.
+	var slice = array[0:n]              //Se devuelve un slice con el valor solicitado
+	generaAleatorio(semilla, n, &slice) //slice se convierte en la lista de valores generados
+	convertidor(&slice)                 //Los valores en slice se traducen al intervalo 0-255
+	return slice
+}
+
+func imprimir(list []int) {
+	//Para imprimir una lista
+	for i := 0; i < len(list); i++ {
+		fmt.Println("Elemento: ", i, " -> ", (list)[i])
+	}
+
+}
+
+//======================================Arbol Binario=========================================================
+
 // Estructura para el nodo de los arboles
 type Nodo struct {
 	llave         int
@@ -112,7 +178,8 @@ func inOrder(raiz *Nodo) {
 	}
 }
 
-//===============================================================================================
+//======================================Balance de Arbol=========================================================
+
 //http://www.smunlisted.com/day-stout-warren-dsw-algorithm.html
 // https://csactor.blogspot.com/2018/08/dsw-day-stout-warren-algorithm-dsw.html
 
